b9c66049f119f33758f60de1324c7ab2bd0bc44c
Alexandr Kosarev <alex7r@ya.ru>
2017-04-05 23:41:08 +0300

    Plugin init
    Initialize Joomla Plugin

--- overrideany.php
+++ overrideany.php
@@ -16,15 +16,20 @@ defined('_JEXEC') or die;
  */
 class PlgSystemOverrideAny extends JPlugin
 {
-	public function onAfterRoute(){
-	    $overridables = array();
-      $overridables['ContentModelCategory']['initial'] = JPATH_BASE.'/components/com_content/models/category.php';
-      $overridables['ContentModelCategory']['target'] = JPATH_BASE.'/core/content/models/category.php';
-      foreach ($overridables as $class => $files){
-          $files['initial'] = str_replace($class, $class.'Base', file_get_contents($files['initial']));
-          $files['initial'] = preg_replace('/^\\<\\?php[^A-z]/','', $files['initial']);
-          eval($files['initial']);
-          JLoader::register($class, $files['target']);
-      }
+    public function onAfterRoute()
+    {
+        $overridables                                    = array();
+        $overridables['ContentModelCategory']['initial'] = JPATH_BASE
+            . '/components/com_content/models/category.php';
+        $overridables['ContentModelCategory']['target']  = JPATH_BASE
+            . '/core/content/models/category.php';
+        foreach ($overridables as $class => $files) {
+            $files['initial'] = str_replace($class, $class . 'Base',
+                file_get_contents($files['initial']));
+            $files['initial'] = preg_replace('/^\\<\\?php[^A-z]/', '',
+                $files['initial']);
+            eval($files['initial']);
+            JLoader::register($class, $files['target']);
+        }
     }
- }
+}
