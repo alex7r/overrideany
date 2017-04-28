ea11d823fe1d25e53559700be097ec88b7930802
Alexandr Kosarev <alex7r@ya.ru>
2017-04-28 13:05:05 +0300

    load class
    fixes Joomla have some core files JLoader::register with force

--- overrideany.php
+++ overrideany.php
@@ -16,14 +16,20 @@ defined('_JEXEC') or die;
  */
 class PlgSystemOverrideAny extends JPlugin
 {
+    protected static $_overridables = array();
+
+    public function __construct($subject, array $config = array())
+    {
+        parent::__construct($subject, $config);
+        self::$_overridables['ContentModelArticles']['initial'] = JPATH_BASE
+            . '/components/com_content/models/articles.php';
+        self::$_overridables['ContentModelArticles']['target']  = JPATH_BASE
+            . '/core/articles.php';
+    }
+
     public function onAfterRoute()
     {
-        $overridables                                    = array();
-        $overridables['ContentModelCategory']['initial'] = JPATH_BASE
-            . '/components/com_content/models/category.php';
-        $overridables['ContentModelCategory']['target']  = JPATH_BASE
-            . '/core/content/models/category.php';
-        foreach ($overridables as $class => $files) {
+        foreach (self::$_overridables as $class => $files) {
             if (!class_exists($class . 'Base', false)) {
                 $files['initial'] = str_replace($class, $class . 'Base',
                     file_get_contents($files['initial']));
@@ -31,6 +37,7 @@ class PlgSystemOverrideAny extends JPlugin
                     $files['initial']);
                 eval($files['initial']);
                 JLoader::register($class, $files['target']);
+                JLoader::load($class);
             }
         }
     }
